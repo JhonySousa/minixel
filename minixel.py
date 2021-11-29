8d5b9e9428f888dda6ee3587ae902c9c4ec50cb0
JhonySousa <ldejcursos@gmail.com>
2021-11-29 14:56:36 -0300

    Improving the printing and minor bugs fixes
    Uh... I'm full of errors now... And I'll finish fixing them later...
    
    First of all, I created the Sheet class, an UserDict to hold our table
    with data and display it (non interactive). This part is functional...
    But the hole program has been modified with little changes on patterns,
    Error messages, and other kind of stuff...
    Now, I'm about to start refactoring and creating a proper project
    structure. I didint done that before because "I'm woking on a little
    project"... Oh dear, now is really big!

--- minixel.py
+++ minixel.py
@@ -1,6 +1,8 @@
-from typing import Any, List, Dict, NamedTuple, NoReturn, Union, Tuple, Callable
+from typing import Any, List, Dict, NamedTuple, NoReturn, Optional, Union, Tuple, Callable
 from operator import add, sub, mul, truediv as div
 from string import ascii_uppercase
+from collections import UserDict
+from functools import reduce
 import argparse
 # import curses
 import os
@@ -77,9 +79,10 @@ class CellFormula:
             List[str]: tokens
         """
         pattern = re.compile(
-            r'([\+\-\*\/\^])|(\(?\)?)|(SUM|SUB|MUL|DIV)\(([A-Z]\d+(?::\d+){0,2})\)|(\d+)'
+            r'(SUM|SUB|DIV|MUL)\(([A-Z]\d+(?::\d+){1,2})\)|' \
+            + r'([A-Z]\d+(?::\d+){0,2})|(\d+)|([\+\-\*\/\^])|(\(?\)?)'
         )
-        setence = [el for el in pattern.split(setence) if el and el != '=']
+        setence = [el for el in pattern.split(setence) if el not in (None, '', '=', ' ')]
         return setence
 
     @staticmethod
@@ -142,6 +145,10 @@ class CellFormula:
         Returns:
             Equation: The hole equation
         """
+        if '(' in tokens:
+            tokens.remove('(')
+        if ')' in tokens:
+            tokens.remove(')')
         pattern = re.compile(r'(?:[A-Z]\d+)|\d+')
         for op in cls.op_dict:
             for _ in range(tokens.count(op)):
@@ -258,11 +265,69 @@ class CellFormula:
         if self.value is None:
             return self.setence
         else:
-            return '='+str(self.value)
+            return '=' + str(self.value)
 
 
 CellType = Union[str, int, CellFormula]
 SheetType = Dict[str, CellType]
+
+
+class Sheet(UserDict):
+    def __init__(self, __dict: Optional[Dict[str, CellType]]=None, **kwargs: CellType) -> None:
+        super().__init__(__dict=__dict, **kwargs)
+        self._size: Optional[Tuple[int, int]] = None
+        self._col_sizes: Dict[str, int] = dict()
+
+    @property
+    def size(self) -> Tuple[int, int]:
+        if self._size is None or self._size == (0, 0):
+            max_row = max_col = 0
+            for elements in self.keys():
+                if elements == '__dict':
+                    continue
+                row, col = coord2index(elements)
+                max_row = max((max_row, row + 1))
+                max_col = max((max_col, col + 1))
+            self._size = (max_row, max_col)
+        return self._size
+
+    @property
+    def col_sizes(self) -> Dict[str, int]:
+        if not self._col_sizes:
+            for key, value in self.items():
+                if key == '__dict':
+                    continue
+                _, col = coord2index(key)
+                if isinstance(value, CellFormula):
+                    self._col_sizes[col] = len(str(value))
+                elif isinstance(value, int):
+                    self._col_sizes[col] = len(str(value))
+                else:
+                    self._col_sizes[col] = len(value)
+        return self._col_sizes
+
+    def display(self):
+        if self.size == (0, 0):
+            return '''
+            +-------+
+            | Empty |
+            +-------+
+            '''
+        index_row_space = len(str(self.size[0])) + 2
+        for row in range(self.size[0] + 1):
+            print(
+                '|\033[90m{0:^{1}}\033[m|'.format(
+                    row if row else ' __',
+                    index_row_space
+                ),
+                end=''
+            )
+            for col in range(self.size[1]):
+                if row == 0:
+                    print('\033[90m', end='')
+                element = str(self.get(index2coord(row - 1, col), 0)) if row else ascii_uppercase[col]
+                print(f'{element:^{self.col_sizes[col] + 2}}\033[m', end='|')
+            print('')
 
 
 def index2coord(row:int, col:int) -> str:
@@ -297,12 +362,12 @@ def coord2index(coord:str) -> Tuple[int, int]:
     Returns:
         Tuple[int, int]: The indexes.
     """
-    row = int(coord[-1:]) - 1
+    row = int(re.match(r'[A-Z](\d+).*', coord).group(1)) - 1
     col = ascii_uppercase.find(coord[0])
     return (row, col)
 
 
-def read_csv(path: str) -> SheetType:
+def read_csv(path: str) -> Sheet:
     """Read the CSV and generates the Table (sheet).
 
     Args:
@@ -313,24 +378,20 @@ def read_csv(path: str) -> SheetType:
     """
     with open(path, 'r') as csv_file:
         content = csv_file.readlines()
-    sheet = dict()
-    max_row = max_col = 0
+    sheet = Sheet()
+    num_pattern = re.compile(r'\-?\d+')
     for row_index, row in enumerate(content):
-        max_row = max((max_row, row_index + 1))
         for col_index, cell in enumerate(row.split(',')):
             cell = cell.strip()
-            if cell.isnumeric():
+            if num_pattern.match(cell):
                 cell = int(cell)
             elif cell.startswith('='):
                 cell = CellFormula(cell)
             sheet[index2coord(row_index, col_index)] = cell
-            max_col = max((max_col, col_index + 1))
-    sheet['ColSpan'] = max_col
-    sheet['RowSpan'] = max_row
     return sheet
 
 
-def display_table(sheet: SheetType) -> NoReturn:
+def interactive_mode(sheet: SheetType) -> NoReturn:
     """Display the table and the data in each cell.
 
     Args:
@@ -345,8 +406,17 @@ def display_table(sheet: SheetType) -> NoReturn:
 def main():
     """Main function"""
     parser = argparse.ArgumentParser()
-    parser.add_argument('-v', '--verbose', help='Show more information.', action='store_true')
-    parser.add_argument('-i', '--interactive', help='Enter interactive mode.', action='store_true')
+    display_group = parser.add_mutually_exclusive_group()
+    display_group.add_argument(
+        '-v', '--verbose',
+        help='Show more information.',
+        action='store_true'
+    )
+    display_group.add_argument(
+        '-i', '--interactive',
+        help='Enter interactive mode.',
+        action='store_true'
+    )
     parser.add_argument(
         'input',
         help="Input file to read data. If dosent exist, the program will create am empty file",
@@ -360,17 +430,17 @@ def main():
     args = parser.parse_args()
     path = args.input
     if not os.path.exists(path):
-        sheet = dict()
-        sheet['ColSpan'] = 3
-        sheet['RowSpan'] = 3
+        sheet = Sheet()
     else:
         sheet = read_csv(path)
+    del(sheet['__dict'])
 
     for el in sheet.values():
         if not isinstance(el, CellFormula):
             continue
         el.compute(sheet)
-    print(sheet)
+    if args.verbose:
+        sheet.display()
 
 
 if __name__ == '__main__':
